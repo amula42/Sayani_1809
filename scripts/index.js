f593a563245b81a9f24f12d31e196294e994bd7c
amula42 <65457511+amula42@users.noreply.github.com>
2021-11-14 21:22:18 +0530

    2022

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Sep 18, 2021',
+  birthdate: 'Sep 18, 2022',
   name: 'SAYANI'
 };
 
